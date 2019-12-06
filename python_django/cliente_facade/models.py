a5c11eac6ec61c4a5bcd35f24a5245e896e87a1f
sandrobraga <sandrobraga@softbox.com.br>
2019-12-06 13:55:50 -0300

    master

--- python_django/cliente_facade/models.py
+++ python_django/cliente_facade/models.py
@@ -4,5 +4,8 @@ class Client(models.Model):
     id   = models.AutoField(primary_key=True)
     name = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.name
+
     def get_name(self):
         return self.name
